9c36e22eab32aa26235ce86673f92afa483a62ce
Miguel Vergara <mverg94@gmail.com>
2023-03-04 18:12:21 +0100

    Finished Homepage, added scroll feature

--- script/script.js
+++ script/script.js
@@ -6,3 +6,30 @@ toggle.addEventListener("click", () =>{
     navMenu.classList.toggle("active");
 })
 /****************************************************************/
+
+/********************Scroll Up Feature************************** */
+const scrollUpBtn = document.getElementById("scrollArrow");
+
+window.onscroll = function () {
+    scrollFunction();
+  };
+
+function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+        scrollUpBtn.style.display = "flex";
+    } else {
+        scrollUpBtn.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  scrollUpBtn.addEventListener("click", backToTop);
+  
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  /*****************************************************************/
