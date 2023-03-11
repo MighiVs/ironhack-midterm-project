ee3f9766c73f0072bc1968d1f560d3cf71b0f7d8
Miguel Vergara <mverg94@gmail.com>
2023-03-11 21:01:25 +0100

    Added contact features, fixed and improved apifetch features (project page)

--- project/apifetch.js
+++ project/apifetch.js
@@ -4,17 +4,15 @@ window.onload = function () {
   fetch(
     "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
   )
-    .then((response) => {
-      if (response) {
-        return response.json();
-      }
-      throw new Error("Network response failed.");
-    })
+    .then(response => response.json())
     .then((data) => {
+
       const mainSection = document.querySelector(".simplify");
       const projectSection = document.querySelector(".projects");
 
-      const others = loadMainProject(mainSection, data, "1");
+      const uuid = (Math.floor(Math.random()*4)+1).toString(); // Random number from 1 to 4
+      
+      const others = loadMainProject(mainSection, data, uuid);
       loadOtherProjects(projectSection, others);
     })
     .catch((error) => {
@@ -24,8 +22,19 @@ window.onload = function () {
 
 // Having the main section, the data, and the uuid, load data in the main section
 // Return the array of the other projects (not main)
-const loadMainProject = (section, apiData, uuid) => {
-  const mainData = apiData.find((project) => project.uuid === uuid); //Find is like filter but returns the first found element
+const loadMainProject = (section, apiData) => {
+  const params = new URLSearchParams(window.location.search);
+  const uuid = params.get("uuid");
+
+  let mainData;
+  // If we have query param, we load that specific project
+  if (uuid) {
+    mainData = apiData.find((project) => project.uuid === uuid);
+    // If we dont, we load a random one
+  } else {
+    const randomIndex = Math.floor(Math.random() * apiData.length);
+    mainData = apiData[randomIndex];
+  }
 
   const template = `
     <h1>${mainData.name}</h1>
@@ -34,32 +43,45 @@ const loadMainProject = (section, apiData, uuid) => {
       <p>${mainData.completed_on}</p>
     </div>
     <div class="container">
-      <img src="${mainData.image}" alt="abstract" />
+      <img src="${mainData.image}" alt="projectPic" />
       <img id="blurred" src="${mainData.image}" alt="" />
       <p>${mainData.content}</p>
     </div>`;
 
   section.innerHTML = template;
 
-  return apiData.filter((project) => project.uuid !== uuid);
+  return apiData.filter((project) => project.uuid !== mainData.uuid);
 };
+
 
 // For every element in otherProjects, add in the proper section said project
 const loadOtherProjects = (section, otherProjects) => {
   otherProjects.forEach((project) => {
     const template = `
     <div class="project">
-      <img src="${project.image}" alt="abstract" />
+      <img src="${project.image}" alt="projectPic" />
       <div class="title">
         <h4>${project.name}</h4>
         <p>${project.description}</p>
       </div>
-      <a href="">Learn more</a>
+      <a href="./project.html?uuid=${project.uuid}">Learn more</a>
     </div>`;
 
     section.innerHTML += template;
+
   });
 };
+
+
+
+
+
+
+
+
+
+
+
 
 /*      NO TEMPLATE METHOD FOR BOTH FUNCTIONS
 // For every element in otherProjects, add in the proper section said project
--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+const SERVER_URL = "http://localhost:8000"
+
 /********************Nav-bar toggle feature**********************/
 const toggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav-links");
@@ -61,9 +63,10 @@ if (
 
 /***************/
 
-/*** Validation for CONTACT FORM ***/
+/*** CONTACT FORM validation and other functionailities ***/
 
 if (window.location.href.includes("contact.html")) {
+
   const contactForm = document.getElementById("contact-us");
 
   const nameInput = document.getElementById("name");
@@ -74,40 +77,22 @@ if (window.location.href.includes("contact.html")) {
   contactForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    // If default html method for validation fails, we check email and phone
+    // Name and message inputs will be free of checking, only "required"
     const errors = {};
+    inputValidation(mailInput, phoneInput, errors);
+    const valid = errorOutput(errors);
 
-    if (nameInput.value.trim() === "") {
-      errors.name = "Name is required.";
-    }
+    if (valid) { 
+      saveContact(nameInput.value, mailInput.value, phoneInput.value, message.value);
+      emptyContactForm(nameInput, mailInput, phoneInput, message);
 
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (mailInput.value.trim() === "") {
-      errors.email = "Email is required.";
-    } else if (!isValidEmail(mailInput.value)) {
-      errors.email = "Please enter a valid email address.";
-    }
-
-    const phoneRegex = /^(\d{3}\s?\d{3}\s?\d{3})$/;
-    if (phoneInput.value.trim() === "") {
-      errors.phone = "Phone number is required.";
-    } else if (!phoneRegex.test(phoneInput.value)) {
-      errors.phone = "Please enter a valid 9-digit phone number.";
-    }
-
-    if (message.value.trim() === "") {
-      errors.message = "Message is required.";
-    }
-
-    if (Object.keys(errors).length > 0) {
-      // Display errors to the user
-      for (var error in errors) {
-        alert(`Error in field "${error.toUpperCase()}": ${errors[error]}`);
-      }
-    } else {
-      // Allow the form to submit
-      contactForm.submit();
+      alert("Thank you for contacting us! We have received your message and will get back to you as soon as possible.");
     }
   });
+
+  
+
 }
 /********************************* */
 
@@ -119,5 +104,70 @@ function isValidEmail(email) {
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return emailRegex.test(email);
 }
+
+function inputValidation(mailInput, phoneInput, errors) {
+  if (mailInput.value.trim() === "") {
+    errors.email = "Email is required.";
+  } else if (!isValidEmail(mailInput.value)) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  const phoneRegex = /^(\d{3}\s?\d{3}\s?\d{3})$/;
+  if (phoneInput.value.trim() === "") {
+    errors.phone = "Phone number is required.";
+  } else if (!phoneRegex.test(phoneInput.value)) {
+    errors.phone = "Please enter a valid 9-digit phone number.";
+  }
+}
+
+function errorOutput(errors) {
+  if (Object.keys(errors).length > 0) {
+    // Display errors to the user
+    for (var error in errors) {
+      alert(`Error in field "${error.toUpperCase()}": ${errors[error]}`);
+    }
+    return false;
+  } else {
+    // Allow the form to submit
+    return true;
+  }
+}
+
+
+
+function saveContact(name, email, phone, message) {
+
+  const contact = {
+    name,
+    email,
+    phone,
+    message
+  }
+
+  fetch(`${SERVER_URL}/contacts`)
+    .then(response => response.json())
+    .then(data => {
+      const contacts = data.contacts || []; // get existing contacts, or start with an empty array
+      contacts.push(contact); // add the new contact to the array
+      return fetch(`${SERVER_URL}/contacts`, {
+        method: 'PUT', // PUT method:  to update the entire contacts array
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ contacts }) // send the updated contacts array back to the server
+      });
+    })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)));
+}
+
+function emptyContactForm(name, email, phone, message) {
+  name.value = "";
+  email.value = "";
+  phone.value = "";
+  message.value = "";
+}
+
+
 
 /*************** */
